d964272d06cb557d6f08997d9a9899c199ed686c
ashish-me-15 <ashish.sharma75.email@gmail.com>
2025-02-21 23:55:28 +0530

    Done with the API integration part

--- src/App.js
+++ src/App.js
@@ -8,6 +8,20 @@ const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
+  useEffect(() => {
+
+    const fetchItems = async () => {
+      const result = await axios (`https://api.breakingbadquotes.xyz/v1/quotes`)
+
+      console.log(result.data)
+      setItems(result.data)
+      setIsLoading(false)
+    }
+
+    fetchItems();
+
+  },[])
+
 
   return (
     <div className="container">
